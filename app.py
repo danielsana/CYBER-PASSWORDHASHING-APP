34754eb5df27bff7a8d9ee2aee2589026e7e698e
dansana <danielsana60@gmail.com>
2024-06-21 10:58:35 +0300

    first commit

--- app.py
+++ app.py
@@ -12,6 +12,25 @@ def signup():
         #Check if form was posted by user
         #create a function
     if request.method == 'POST':
+            
+            #get form data
+            username = request.form['username']
+            email = request.form['email']
+            password = request.form['password']
+            title = request.form['title']
+
+            connection =pymysql.connect(host='localhost',user='root',password='',database='cyberdb')
+
+            cursor = connection.cursor()
+
+            sql ='''
+                insert into users(username,email,password,title)values(%s,%s,%s,%s)
+                '''
+            
+            cursor.execute(sql,(username,email,password,title))
+
+            connection.commit()
+            
             return render_template('signup.html', msg='Application Made Successfully')
     else:
         # Form not posted, display the form to allow user Post something
